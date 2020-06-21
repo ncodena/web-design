d8bff9c5840937b631a43b7506caaa1546d2ecf6
Nuria <c.odena.nuria@gmail.com>
2020-06-21 22:09:06 +0200

    displaying scroll reveal

--- main.js
+++ main.js
@@ -13,26 +13,26 @@ sr.reveal('.heading, .heading-underline', {
     origin: 'left',
     duration: '1500',
     distance: '25rem',
-    delay: 300
+    delay: 200
 });
 
 sr.reveal('.animate-right', {
     origin: 'right',
     duration: '1500',
     distance: '25rem',
-    delay: 600
+    delay: 400
 });
 
 sr.reveal('.animate-bottom', {
     origin: 'bottom',
     duration: '1500',
     distance: '25rem',
-    delay: 300
+    delay: 400
 });
 
 sr.reveal('.animate-top', {
     origin: 'bottom',
     duration: '1500',
     distance: '25rem',
-    delay: 600
+    delay: 400
 });
