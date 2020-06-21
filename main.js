bec07a309e123e9197ae2fd3954d33134eab3ff5
Nuria <c.odena.nuria@gmail.com>
2020-06-21 11:18:57 +0200

    displaying scroll reveal

---
+++ main.js
@@ -0,0 +1,17 @@
+// Scroll reveal
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: '1500',
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: '1500',
+    distance: '25rem',
+    delay: 600
+});
